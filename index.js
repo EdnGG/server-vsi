4842bcc717007face3f5dccc750e28c37ceeb16a
Eden Gomez <gresseden@gmail.com>
2024-02-09 21:13:03 -0500

    saving changes on server and getting ready to new branch

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require('express')
+
 const app = express()
 const cors = require('cors')
-// const userRoutes = require('./routes/users')
 const chatRoutes = require('./routes/chatGpt')
-// const connectDB = require('./config/db')
 
 require('dotenv').config()
-// connectDB()
 
 app.use(cors())
 app.use(express.json())
 
 //3. Rutas
-// app.use('/usuario', userRoutes)
 app.use('/chat', chatRoutes)
 app.get('/', (req, res) => res.send('VSI CHATBOT API'))
 
